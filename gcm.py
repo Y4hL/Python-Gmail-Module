8ead8dff73193596fafe02997eca13bc30e812d1
Y4hL <43219534+Y4hL@users.noreply.github.com>
2019-03-12 22:04:33 +0200

    Bug Fixes and more
    Changed a bit of the formatting and squashed a few bugs

--- gcm.py
+++ gcm.py
@@ -16,7 +16,8 @@ import base64
 import email, imaplib, os, math, time
 
 
-def auth(user, password, imap_url="imap.gmail.com"): # Google Server Authentication
+def auth(user, password, imap_url="imap.gmail.com", inbox="INBOX"): # Google Server Authentication
+    # returns a connection
 
     if type(user) == bytes:
         user = user.decode()
@@ -24,29 +25,30 @@ def auth(user, password, imap_url="imap.gmail.com"): # Google Server Authenticat
         password = password.decode()
     con = imaplib.IMAP4_SSL(imap_url)
     con.login(user, password)
+    con.select(inbox)
     return con
 
 
-def get_mail_ids(con, inbox="INBOX"):
+def get_mail_ids(con):
+    # Gets a list of all mail ids in a inbox
 
-    con.select(inbox)
     _, get_latest_data = con.search(None, "ALL")
     mail_id_str = get_latest_data[0] # get_latest_data is a list
     return mail_id_str.split(b' ')
 
 
-def get_email_body(con, mail_id, inbox="INBOX"):
+def get_email_body(con, mail_id):
+    # Gets the body of a given mail_id
 
-    mail_id_list = get_mail_ids(con, inbox=inbox)
+    mail_id_list = get_mail_ids(con)
 
     if type(mail_id) != bytes:
         raise ValueError("mail_id should be a bytes object")
 
     if not mail_id in mail_id_list:
-        raise Exception("mail id '{}' does not correspond to a email in {}".format(mail_id, inbox))
+        raise Exception("mail id '{}' does not correspond to a email".format(mail_id))
 
-    con.select(inbox)
-    mail_message = con.fetch(mail_id, "(RFC822)")
+    _, mail_message = con.fetch(mail_id, "(RFC822)")
     raw = email.message_from_bytes(mail_message[0][1])
     for part in raw.walk():
         if part.get_content_type() == 'text/plain':
@@ -58,15 +60,15 @@ def get_email_body(con, mail_id, inbox="INBOX"):
             return body
 
 
-def get_mail(con, mail_id, inbox="INBOX"):
+def get_mail(con, mail_id):
     # Same as list_mails function, but only returns it for one given mail
 
     if type(mail_id) != bytes:
         raise ValueError("mail_id should be a bytes object")
 
-    mail_id_list = get_mail_ids(con, inbox=inbox)
+    mail_id_list = get_mail_ids(con)
     if not mail_id in mail_id_list:
-        raise Exception("mail id '{}' does not correspond to a email in {}".format(mail_id, inbox))
+        raise Exception("mail id '{}' does not correspond to a email".format(mail_id))
     _, mail_message = con.fetch(mail_id, "(RFC822)")
     str_message = mail_message[0][1].decode("utf-8")
     email_message = email.message_from_string(str_message)
@@ -75,21 +77,22 @@ def get_mail(con, mail_id, inbox="INBOX"):
     date = email_message['Date'] # Gets Date Email was sent
     recipiant = email_message['To'] # Gets Email Recipiant
 
-    body = get_email_body(mail_id, con) # Gets Body
+    body = get_email_body(con, mail_id) # Gets Body
 
     message = {
-            "id": mail_id,
-            "subject": email_subject,
-            "from": email_sender,
-            "date": date,
-            "to": recipiant,
-            "body": body
+            "Id": mail_id,
+            "Subject": email_subject,
+            "From": email_sender,
+            "Date": date,
+            "To": recipiant,
+            "Body": body
         }
     return message
 
-def list_mails(con, inbox="INBOX"):
+def list_mails(con):
+    # returns a list of dictionarys ( each dictinary is a mail )
 
-    mail_id_list = get_mail_ids(con, inbox=inbox)
+    mail_id_list = get_mail_ids(con)
 
     messages = []
 
@@ -102,7 +105,7 @@ def list_mails(con, inbox="INBOX"):
         date = email_message['Date'] # Gets Date Email was sent
         recipiant = email_message['To'] # Gets Email Recipiant
 
-        body = get_email_body(mail_id, con) # Gets Body
+        body = get_email_body(con, mail_id) # Gets Body
 
         messages.append({
             "id": mail_id,
@@ -115,10 +118,11 @@ def list_mails(con, inbox="INBOX"):
     return messages
 
 
-def attachment_state(con, mail_id, inbox="INBOX"):
-    
-    con.select(inbox)
-    mail_id_list = get_mail_ids(con, inbox=inbox)
+def attachment_state(con, mail_id):
+    # Returns False if no attachment is found
+    # or the file name if one is found
+
+    mail_id_list = get_mail_ids(con)
     
     if type(mail_id) != bytes:
         raise ValueError("mail_id should be a bytes object")
@@ -138,6 +142,7 @@ def attachment_state(con, mail_id, inbox="INBOX"):
 
 
 def send_gmail(email, password, send_to_email, subject, message, file_location=False):
+    # Returns True if email was sent successfully
     
     if type(email) != str:
         raise ValueError("email should be a string")
@@ -180,16 +185,16 @@ def send_gmail(email, password, send_to_email, subject, message, file_location=F
     return True
 
 
-def get_attachment(con, mail_id, save_dir, inbox="INBOX"):
+def get_attachment(con, mail_id, save_dir):
+    # Returns True or False, depending if a attachment was downloaded
     
-    con.select(inbox)
-    mail_id_list = get_mail_ids(con, inbox=inbox)
+    mail_id_list = get_mail_ids(con)
     
     if type(mail_id) != bytes:
         raise ValueError("mail_id should be a bytes object")
     
     if not mail_id in mail_id_list:
-        raise Exception("mail id '{}' does not correspond to a email in {}".format(mail_id, inbox))
+        raise Exception("mail id '{}' does not correspond to a email".format(mail_id))
     _, mail_message = con.fetch(mail_id, "(RFC822)")
     raw = email.message_from_bytes(mail_message[0][1])  # gets email from list
     for part in raw.walk():
