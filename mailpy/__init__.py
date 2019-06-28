d3ec75ead0b4f4b4a4e786349d1783e861ef00b7
Y4hL <43219534+Y4hL@users.noreply.github.com>
2019-06-28 17:43:42 +0300

    Started Adding CLI

--- mailpy/__init__.py
+++ mailpy/__init__.py
@@ -5,7 +5,12 @@ __version__ = '.'.join(map(str, VERSION))
 __author__ = 'https://github.com/Y4hL'
 __license__ = 'GNU GPLv3'
 __copyright__ = 'Copyright 2019 Y4hL'
-
+__doc__ = 'Easily Send and Receive Mails with Python. Conveniently Send and Download Attachements '
 
 from .Gmail import *
 from .exceptions import *
+
+
+def cli():
+
+    print('CLI for mailpy will be added soon!')
