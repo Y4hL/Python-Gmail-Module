1d127d968e80ea8467cfdcfac3beb13807f2a280
Y4hL <43219534+Y4hL@users.noreply.github.com>
2019-07-13 12:05:22 +0200

    Improved Example

--- examples/delete_mail.py
+++ examples/delete_mail.py
@@ -16,14 +16,9 @@ if __name__ == '__main__':
     MAIL_IDS = Client.get_mail_ids()
 
     # Check if MAIL_IDS is empty
-    if MAIL_IDS == []: 
+    if MAIL_IDS != []:
 
-        # Raise Exception if there are no emails
-        raise Exception('You have no mails in your Indox')
-
-    else:
-
-         # Deletes latest mail
+        # Deletes latest mail
         Client.delete_mail(MAIL_IDS[0])
 
     # Disconnect Client
